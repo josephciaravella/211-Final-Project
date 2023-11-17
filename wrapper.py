fc8bf9765d0aa16f2b0c5ad9cb540404687eda47
Joseph Ciaravella <joseph.ciaravella@gmail.com>
2023-11-17 11:16:30 -0500

    wrapper changes

--- wrapper.py
+++ wrapper.py
@@ -13,54 +13,129 @@ right_wheel = Motor("B")
 color_sensor_left = EV3ColorSensor(3)
 color_sensor_right = EV3ColorSensor(2)          
 FIRE_POSITION_DICT = {'A':1, 'B':2, 'C':3, 'D':4, 'E':5, 'F':6}
+orientation = "up"
 
 
 
-def determine_move(prev_pos, curr_pos, direction):
+def determine_move(curr_pos, next_pos):
+    """
+    Determines the move direction based on the current position and the next position.
 
+    Parameters:
+    - curr_pos (tuple): A tuple representing the current position (x, y).
+    - next_pos (tuple): A tuple representing the next position (x, y).
 
-    return move, direction;
+    Returns:
+    - move (str): A string representing the direction of the move. Possible values are "north", "south", "east", or "west".
+    """
+    # Calculate the change in x and y coordinates
+    delta_x = next_pos[0] - curr_pos[0]
+    delta_y = next_pos[1] - curr_pos[1]
+
+    # Combine the changes into a tuple representing the move coordinates
+    move_coord = (delta_x, delta_y)
+
+    # Define a dictionary mapping move coordinates to direction strings
+    move_dict = {(0, 1): "north", (0, -1): "south", (1, 0): "east", (-1, 0): "west"}
+
+    # Retrieve the direction string based on the move coordinates
+    move = move_dict[move_coord]
+
+    # Return the determined move direction
+    return move
 
 
 def navigation(path):
-    direction = "up"
-    curr_pos = [0,0]
-    move = determine_move()
+    """
+    Navigates a path in a grid based on specified moves, updating the current position and orientation.
 
-    if move == "left":
-        curr_pos[0] -= 1
-        if direction == "left":
-            # go straight
-        elif direction == "right":
-            # rotate left x2
-            # go straight
-        elif direction == "up":
-            # rotate left
-            # go straight
-        elif direction == "down":
-            # rotate right
-            # go straight
+    Parameters:
+    - path (list of tuples): A list of tuples representing the path to navigate. Each tuple is a coordinate (x, y).
+
+    Returns:
+    - orientation (string): A string representing the orientation of the robot once it reaches the end of its path
+
+    Notes:
+    - The function modifies the current position and orientation based on the given path.
+    - The orientation is initially set to "up" and is updated as the navigation progresses.
+    - The function includes placeholder comments for specific actions to be taken at each step in the grid.
+    """
+
+    
+    curr_pos = [0,0]
+    i=0
+    while (i+1 <= len(path)):
+        move = determine_move(curr_pos, path[i+1])
+
+
+        if move == "west":
+            curr_pos[0] -= 1
+            if orientation == "left":
+                # go straight
+            elif orientation == "right":
+                # rotate left x2
+                # go straight
+            elif orientation == "up":
+                # rotate left
+                # go straight
+            elif orientation == "down":
+                # rotate right
+                # go straight
+
+            orientation = "left"
+            
+
+        if move == "east":
+            curr_pos[0] += 1
+            if orientation == "left":
+                # rotate left x2
+                # go straight
+            elif orientation == "right":
+                # go straight
+            elif orientation == "up":
+                # rotate right
+                # go straight
+            elif orientation == "down":
+                # rotate left
+                # go straight
+
+            orientation = "right"
         
 
+        if move == "north":
+            curr_pos[1] += 1
+            if orientation == "left":
+                # rotate right
+                # go straight
+            elif orientation == "right":
+                # rotate left
+                # go straight
+            elif orientation == "up":
+                # go straight
+            elif orientation == "down":
+                # rotate left x2
+                # go straight
 
-    if move == "right":
-        curr_pos[0] += 1
-        if direction == "left":
-            # rotate left x2
-            # go straight
-        elif direction == "right":
-            # go straight
-        elif direction == "up":
-            # rotate right
-            # go straight
-        elif direction == "down":
-            # rotate left
-            # go straight
-    
-    if move == "up":
-        curr_pos[1] += 1
-        #go straight 
-        #curr_pos[1] += 1
+            orientation = "up"
+
+            
+        if move == "south":
+            curr_pos[1] -= 1
+            if orientation == "left":
+                # rotate left
+                # go straight
+            elif orientation == "right":
+                # rotate right
+                # go straight
+            elif orientation == "up":
+                # rotate right x2
+                # go straight
+            elif orientation == "down":
+                # go straight
+
+            orientation = "down"
+
+    return orientation
 
 
 
